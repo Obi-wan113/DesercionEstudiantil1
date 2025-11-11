f1e04972b30e5758bbbac1ca94de63f922621f3d
DieCs2806 <diegofelipecespedes5@gmail.com>
2025-11-10 21:17:38 -0500

    extender los atributos de desercion

--- analysis.py
+++ analysis.py
@@ -3,3 +3,17 @@ def dropout_rate(df):
   dropped = len(df[df["status"] == "Dropped"])
   rate = dropped / total * 100
   return round(rate, 2)
+
+def proporcion_desercion(df):
+    dropped_df = df[df["status"] == "Dropped"]
+
+    if dropped_df.empty:
+        return 0, None 
+    
+    total = len(df)
+    dropped = len(dropped_df)
+    rate = round(dropped / total * 100, 2)
+
+    common_cause = dropped_df["cause"].mode()[0]  
+
+    return rate, common_cause
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from data_loader import load_data
-from analysis import dropout_rate
+from analysis import dropout_rate, proporcion_desercion
 if __name__ == "__main__":
   data = load_data()
   rate = dropout_rate(data)
+
+  prop = proporcion_desercion(data)
   print(f"Dropout rate: {rate}%")
+  print(f"Dropout proportion: {prop[0]}%")
+  if prop[1]:
+      print(f"Most common cause of dropout: {prop[1]}")
+
+ 
